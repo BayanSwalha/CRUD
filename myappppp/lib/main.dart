c44b4d4e509806facbe2857824342aa52e6419d3
BayanSwalha <bayanswalha114@gmail.com>
2022-10-16 16:09:37 +0300

    upload

--- myappppp/lib/main.dart
+++ myappppp/lib/main.dart
@@ -2,7 +2,6 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:myappppp/firebase_options.dart';
 
-import 'CURD/read.dart';
 import 'LogIn.dart/login.dart';
 
 Future<void> main() async {
@@ -21,6 +20,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListPage();
+    return LogIn();
   }
 }
